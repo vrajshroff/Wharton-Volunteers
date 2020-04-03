0096ce7292504b614bc6a5d36095b1cda4b334ab
Vraj R Shroff <35973010+vrajshroff@users.noreply.github.com>
2020-04-03 15:34:01 -0500

    update

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', function(req, res) {
@@ -14,5 +22,14 @@ app.get('/auth', function(req, res) {
 })
 
 app.get('/create', function(req, res) {
+  res.render(path.join(__dirname + '/create.ejs'));
+})
+
+app.get('/temp', function(req, res) {
+  res.sendFile(path.join(__dirname + '/temp.html'));
+})
+
+app.post('/addHospital', function(req, res) {
+  console.log(req.body);
   res.render(path.join(__dirname + '/create.ejs'));
 })
